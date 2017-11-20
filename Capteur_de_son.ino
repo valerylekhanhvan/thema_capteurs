21b39f6f28a0cf701a79a7e708270525e516ee09
MatisMessence <32521194+MatisMessence@users.noreply.github.com>
2017-11-20 17:51:13 +0100

    Add files via upload

---
+++ Capteur_de_son.ino
@@ -0,0 +1,31 @@
+const int pinsensor = A0;
+const int pinbutton = 1;
+bool button = 0;
+
+void setup()
+{
+    Serial.begin(115200);
+    pinMode (1 ,OUTPUT);
+}
+
+void loop()
+{
+  if (digitalRead(pinbutton) == HIGH)
+  {
+    button ++;
+    if (button == 1)
+    {
+      long sum = 0;
+      for(int i=0; i<32; i++)
+      {
+        sum += analogRead(pinsensor);
+      }
+      
+      sum >>= 5;
+      long value = sum / 10;
+      Serial.print(value);
+      Serial.println("dB");
+    }
+    delay(100);
+  }
+}
